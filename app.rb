ae27c20152945aad79b57aaf103f0354d99ee79f
Denise Pendleton <dpendl1@hotmail.com>
2018-04-15 17:53:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
-require './environment'
+require_relative './environment.rb'
+# require_relative 'views/pirates/new.erb'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
-
     # code other routes/actions here
+
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+     erb  :new
+  end
+
+  post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+    # binding.pry
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+
+    @ships = Ship.all
+    erb :show
+  end
+
 
   end
 end
---
+++ models/ship.rb
@@ -0,0 +1,23 @@
+class Ship
+
+
+  SHIPS = []
+  def initialize(args)
+    @name=args[:name]
+    @type=args[:type]
+    @booty=args[:booty]
+      SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
+
+
+
+end
