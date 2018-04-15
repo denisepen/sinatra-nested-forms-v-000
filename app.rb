1dab85eaa8882306aac5973168c1b291761c9cc9
Denise Pendleton <dpendl1@hotmail.com>
2018-04-15 18:17:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,10 @@ module FormsLab
 
   post '/pirates' do
     @pirate = Pirate.new(params[:pirate])
-  
+
     params[:pirate][:ships].each do |details|
       Ship.new(details)
+      binding.pry
     end
 
     @ships = Ship.all
